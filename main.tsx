cc582e12e83991d6f14590ee88cd4064b515e454
KungKrister <143955085+KungKrister@users.noreply.github.com>
2025-04-16 13:24:38 +0200

    Add files via upload

---
+++ main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import RobotButikk from './RobotButikk';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RobotButikk />
+  </React.StrictMode>,
+);
